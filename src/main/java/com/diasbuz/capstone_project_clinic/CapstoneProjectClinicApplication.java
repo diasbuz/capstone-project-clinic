00abd17374074ee7d8c780fc424f3250b94dc4d7
diasbuz <grandleocraft@gmail.com>
2024-05-31 20:32:28 +0600

    Init commit.

---
+++ src/main/java/com/diasbuz/capstone_project_clinic/CapstoneProjectClinicApplication.java
@@ -0,0 +1,13 @@
+package com.diasbuz.capstone_project_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diasbuz/capstone_project_clinic/CapstoneProjectClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diasbuz.capstone_project_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
